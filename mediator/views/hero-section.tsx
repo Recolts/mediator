41592d51e6dd6571094831f6314b3dd52e77bc4b
Ayrown <daveavrosia@gmail.com>
2024-08-07 23:01:53 +0800

    Update input component

--- mediator/views/hero-section.tsx
+++ mediator/views/hero-section.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { ComboboxDemo } from "@/components/ui/combobox";
 import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import React from "react";
@@ -18,12 +19,19 @@ const HeroSection = () => {
         </div>
 
         <div className="flex justify-center items-center gap-4">
-          <Input
-            title="private key"
-            placeholder="Paste an escrow private key here..."
-          />
+          <div className="flex p-1 border rounded-xl w-[344px] border-white-8">
+            <Input
+              title="private key"
+              placeholder="Paste an escrow private key here..."
+              className="border rounded-lg p-3.5 border-white-8"
+            />
+          </div>
           <h1 className="ty-subtext text-white-12">OR</h1>
-          <Button variant={"default"}>Create an Escrow</Button>
+          <div className="flex p-1 border rounded-xl border-blue-50">
+            <Button variant={"default"} className="ty-title p-3.5">
+              Create an Escrow
+            </Button>
+          </div>
         </div>
         <div className="flex gap-4 max-w-[896px] w-full">
           <div className="flex gap-4">
@@ -40,7 +48,9 @@ const HeroSection = () => {
               </TabsContent> */}
             </Tabs>
           </div>
-          <div className="flex bg-white-100">Hello</div>
+          <div className="flex">
+            <ComboboxDemo />
+          </div>
         </div>
       </div>
     </div>
