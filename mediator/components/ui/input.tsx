de355fbbb4bf83bdb46f50a7d65a246cd0427baf
Ayrown <daveavrosia@gmail.com>
2024-08-21 00:33:44 +0800

    Update input

--- mediator/components/ui/input.tsx
+++ mediator/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex w-full rounded-md border border-input bg-background px-3 py-2 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 selection:bg-white-100 selection:text-black-100",
+          "flex file:bg-transparent file:text-sm file:font-medium focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 selection:bg-white-100 selection:text-white-100",
           className
         )}
         ref={ref}
